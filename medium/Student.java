f823812da04383ed45211c2c6653d747ad63c757
Sameer631100 <sameersardana926@gmail.com>
2025-04-23 19:53:58 +0530

    Create Student.java

---
+++ medium/Student.java
@@ -0,0 +1,19 @@
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private int id;
+    private String name;
+    private double gpa;
+
+    // Constructor
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getGpa() { return gpa; }
+}
